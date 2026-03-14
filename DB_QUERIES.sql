a40fc10fb4436f35255e65fc05728e8c5e1defa9
SharvariShalgar20 <shalgarsharvari@gmail.com>
2026-03-14 23:05:36 +0530

    feat: add SQL setup and common queries for Expense Manager

---
+++ DB_QUERIES.sql
@@ -0,0 +1,51 @@
+-- 1. Create DataBase
+
+CREATE DATABASE IF NOT EXISTS expense_manager
+       CHARACTER SET utf8mb4
+       COLLATE utf8mb4_unicode_ci;
+
+USE expense_manager;
+
+
+-- 2. Create Tables
+
+CREATE TABLE IF NOT EXISTS users (
+    user_id    INT           NOT NULL AUTO_INCREMENT,
+    sername   VARCHAR(50)    NOT NULL UNIQUE,
+    password   VARCHAR(255)  NOT NULL,
+    email      VARCHAR(100)  NOT NULL UNIQUE,
+    currency   VARCHAR(10)   NOT NULL DEFAULT 'INR',
+    created_at TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id)
+    );
+
+
+CREATE TABLE IF NOT EXISTS expenses (
+    expense_id   INT            NOT NULL AUTO_INCREMENT,
+    user_id      INT            NOT NULL,
+    title        VARCHAR(100)   NOT NULL,
+    amount       DECIMAL(10,2)  NOT NULL CHECK (amount > 0),
+    category     VARCHAR(30)    NOT NULL,
+    expense_date DATE           NOT NULL,
+    description  VARCHAR(255)   DEFAULT '',
+    payment_mode VARCHAR(30)    NOT NULL,
+    is_recurring BOOLEAN        NOT NULL DEFAULT FALSE,
+    created_at   TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (expense_id),
+    CONSTRAINT fk_expense_user FOREIGN KEY (user_id)
+    REFERENCES users(user_id) ON DELETE CASCADE
+    );
+
+CREATE TABLE IF NOT EXISTS budgets (
+    budget_id    INT           NOT NULL AUTO_INCREMENT,
+    user_id      INT           NOT NULL,
+    category     VARCHAR(30)   NOT NULL,
+    month        INT           NOT NULL CHECK (month BETWEEN 1 AND 12),
+    year         INT           NOT NULL,
+    limit_amount DECIMAL(10,2) NOT NULL CHECK (limit_amount > 0),
+    created_at   TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (budget_id),
+    CONSTRAINT fk_budget_user FOREIGN KEY (user_id)
+    REFERENCES users(user_id) ON DELETE CASCADE,
+    CONSTRAINT uq_budget UNIQUE (user_id, category, month, year)
+    );
